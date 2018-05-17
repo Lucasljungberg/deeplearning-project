e97483d1a8f52038b8816503dd7626398b36d95b
Lucasljungberg <lucasljungberg@gmail.com>
2018-05-18 01:01:00 +0200

    Temporary storage so I don't lose things

---
+++ cmd.py
@@ -0,0 +1,64 @@
+import sys
+
+class CmdArgs:
+
+    valid_models = [
+        'vgg16',
+        'vgg19'
+    ]
+
+    valid_modes = [
+        'train',
+        'evaluate'
+    ]
+
+    valid_flags = [
+        '--plot',
+        '--save-train-data'
+    ]
+
+    def __init__ (self):
+        self.model = None
+        self.mode = None
+        self.plot = False
+        self.save_train_data = False
+
+    def set (self, flag):
+        if flag == '--save-train-data':
+            self.save_train_data = True
+        elif flag == '--plot':
+            self.plot = True
+
+def validate_cmdargs (args):
+    cmdargs = CmdArgs()
+    def print_usage ():
+        print('Usage: python main.py <%s> <%s> [--plot] [--save-train-data]' 
+            %('|'.join(CmdArgs.valid_modes), '|'.join(CmdArgs.valid_models)))
+
+    # Exit early if no mode is given
+    if len(sys.argv) < 3:
+        print('A mode and model needs to be specified')
+        print_usage()
+        exit()
+
+    arg1 = sys.argv[1]
+    if arg1 not in ['train', 'evaluate']:
+        print('Unknown mode', arg1)
+        print_usage()
+        exit()
+    cmdargs.mode = arg1
+
+    arg2 = sys.argv[2]
+    if arg2 not in CmdArgs.valid_models:
+        print('Unknown model', arg2)
+        print_usage()
+        exit()
+    cmdargs.model = arg2
+
+    for arg in sys.argv[3:]:
+        if arg not in CmdArgs.valid_flags:
+            print('Unknown option', arg)
+            print_usage()
+            exit()
+        cmdargs.set(arg)
+    return cmdargs
--- main.py
+++ main.py
@@ -9,62 +9,60 @@ import os
 import os.path
 import tensorflow as tf
 import util
-
-
-# Weights file path
-weight_file = 'weights/vgg16_transfer.h5'
+import cmd
 
 # Silence debug-info (relevant for tensorflow-gpu)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 config = tf.ConfigProto()
 config.gpu_options.allocator_type = 'BFC'
 
-# Exit early if no mode is given
-if len(sys.argv) < 2:
-    print('A mode needs to be specified')
-    print('Usage: python main.py [train|evaluate]')
-    exit()
+args = cmd.validate_cmdargs(sys.argv)
 
 # Consider splitting some training data into validation data
 (x_train, y_train), (x_test, y_test) = cf10.load_data()
 
-# Parameters
+# Training Parameters
 NUM_CLASSES = 10
 batch_size = 32
 updates_per_epoch = len(x_train) / batch_size
-epochs = 1
-
-# Add padding to the inner parts of the input to fit the 48x48 minimum requirement
-# Using 'mean' strategy
-x_train = np.pad(x_train, [(0, 0), (8, 8), (8, 8), (0, 0)], 'mean')
-x_test = np.pad(x_test, [(0, 0), (8, 8), (8, 8), (0, 0)], 'mean')
+epochs = 50
 
 # Turn labels into one-hot encodings
 y_train = npu.to_categorical(y_train, NUM_CLASSES)
 y_test = npu.to_categorical(y_test, NUM_CLASSES)
 
-# Fetch the model (or create it if there is none)
-model = util.get_model()
+# Fetch the specified model (or create it if there is none)
+model = util.get_model(args.model)
 
 # Data batch generator
 datagen = im.ImageDataGenerator()
 
 # Tensorflow with GPU enabled easily runs out of memory. 
 # So we need to train and evaluate seperately
-mode = os.sys.argv[1]
-if mode == 'train':
+if args.mode == 'train':
 
     # Computes necessary details for feature normalizations (flip/rotate/shift/etc)
     datagen.fit(x_train)
 
-    model.fit_generator(
+    # Test-data datagen validator
+    testdatagen = im.ImageDataGenerator()
+
+    res = model.fit_generator(
         datagen.flow(x_train, y_train, batch_size),
+        validation_data = testdatagen.flow(x_test, y_test, batch_size),
         steps_per_epoch = updates_per_epoch,
         epochs = epochs)
-    util.save_model(model)
-elif mode == 'evaluate':
+    util.save_model(model, args.model)
+    
+    if args.save_train_data:
+        util.save_train_data(model.name, res.history)
+    if args.plot:
+        util.plot(res.history)
+elif args.mode == 'evaluate':
+    accuracy = []
     for i in range(100):
         x_batch = x_test[i*10:(i+1)*10]
         y_batch = y_test[i*10:(i+1)*10]
         res = model.test_on_batch(x_batch, y_batch)
-        print(res)
+        accuracy.append(res[1])
+    print("Average accuracy:", sum(accuracy) / len(accuracy))
